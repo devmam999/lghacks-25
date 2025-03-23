d00a859f65f55a4105d820ae3d9bd8673ede00ea
devmam999 <devmam999@gmail.com>
2025-03-23 10:15:44 -0700

    commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -63,7 +63,7 @@ const Homepage = () => {
                         type="text"
                         id="timeInput"
                         placeholder="Time needed (e.g., 2 hours)"
-                        className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 mb-6 bg-white dark:bg-gray-500 dark:border-gray-700 dark:text-white"
+                        className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 mb-6 bg-white dark:bg-gray-800 dark:border-gray-700 dark:text-white"
                         value={time}
                         onChange={(e) => setTime(e.target.value)}
                     />
@@ -72,7 +72,7 @@ const Homepage = () => {
                     <div className="flex justify-center space-x-4">
                         {/* Add Task Button (Blue) */}
                         <button
-                            className="px-8 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 hover:scale-105"
+                            className="px-8 py-3 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 hover:scale-105"
                             onClick={handleAddTask}
                         >
                             Add Task
@@ -80,7 +80,7 @@ const Homepage = () => {
 
                         {/* Finish Button (Green) */}
                         <button
-                            className="px-8 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300 hover:scale-105"
+                            className="px-8 py-3 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300 hover:scale-105"
                             onClick={handleFinish}
                         >
                             Finish
